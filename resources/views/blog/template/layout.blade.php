06fe0889353cad4126bb0369197ec601e27953f7
MarcoHernandez02 <marco_1320114125@uptecamac.edu.mx>
2022-02-01 19:01:54 -0600

    Blog EM PCS

---
+++ resources/views/blog/template/layout.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+   <title>PROGRAMACION CLIENTE SERVIDOR</title>
+   <!-- Google Fonts -->
+   <link href='http://fonts.googleapis.com/css?family=Raleway:400,800,500,600,300,700' rel='stylesheet' type='text/css'>
+   <!-- ionicons Fonts for icons -->
+   <link href="css/ionicons.min.css" rel="stylesheet">
+   <!-- bootstrap -->
+   <link href="css/bootstrap.css" rel="stylesheet">
+   <!-- Styles CSS-->
+   <link href="css/style.css" rel="stylesheet">
+   <!-- Animate CSS-->
+   <link href="css/animate.css" rel="stylesheet">
+   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+   <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+</head>
+
+<body class="box-layout">
+   <!-- Page Loader -->
+   <div class="page-loader">
+      <div class="loader">
+         <span class="cssload-loading"></span>
+      </div>
+   </div>
+   <!-- End Page Loader -->
+   <div class="inner-conterner">
+      <!-- inner-conterner -->
+      <header>
+      @yield('titulo')
+      </header>
+      <header class="site-header">
+         <div class="header-inner">
+            <!-- navigation panel -->
+            <div class="container">
+               <div class="row">
+                  <div class="header-table col-md-12">
+                     <div class="brand">
+                        <a href="{{route('inicio')}}">
+                           <img src="images/logo.png" alt="Enfold">
+                        </a>
+                     </div>
+                     <nav id="nav-wrap" class="main-nav">
+                        <div id="mobnav-btn"> </div>
+                        <ul class="sf-menu">
+                           <li class="current">
+                              <a href="{{route('inicio')}}">Inicio</a>
+                           </li>
+                           <li>
+                              <a href="{{route('unidades')}}">Unidad 1</a>                             
+                           </li>
+                           <li>
+                              <a href="{{route('contacto')}}">contacto </a>
+                           </li>
+                          
+                           <!--<li class="menu-search-bar">
+                              <a id="search-label"><i class="ion-search"></i></a>
+                              <form class="search-bar" id="searchform">
+                                 <input type="text" placeholder="Search..." name="s">
+                              </form>-->
+                           </li>
+                        </ul>
+                     </nav>
+                  </div>
+               </div>
+               <!-- End navigation panel -->
+            </div>
+            <!-- End navigation panel -->
+         </div>
+      </header>
+
+    @yield('contenido')
+
+    @yield('formulario')
+
+    @yield('topic')
+
+    @yield('aboutus')
+
+          <!-- Footer -->
+          <div class="footer-holder">
+         <div class="container">
+            <footer class="site-footer">
+               <div class="row">
+                  <div class="col-md-6">
+                     <p>© Copyright 2022 | <a href="http://uptecamac.edomex.gob.mx/" target="_blank">Universidad Politécnica de Tecámac</a></p>
+                  </div>
+                  <div class="col-md-6">
+                     <ul class="footer-nav">
+                        <li><a href="#">Bibliografías</a></li>
+                        <li><a href="{{route('personal')}}">Sobre nosotros</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </footer>
+         </div>
+      </div>
+      <!-- End Footer -->
+   </div>
+   <!-- End inner-conterner -->
+   <!-- ================================================== -->
+   <!-- Placed js files at the end of the document so the pages load faster -->
+   <script src="js/jquery.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+   <script src="js/superfish.js"></script>
+   <script src="js/jquery.easing.js"></script>
+   <script src="js/wow.js"></script>
+   <script src="js/jquery.isotope.min.js"></script>
+   <script src="js/owl.carousel.min.js"></script>
+   <script src="js/jquery.magnific-popup.js"></script>
+   <script src="js/jflickrfeed.min.js"></script>
+   <script src="js/jquery.fitvids.js"></script>
+   <script src="js/jquery.fractionslider.min.js"></script>
+   <script src="js/jquery-ui-1.10.4.custom.min.js"></script>
+   <script type="text/javascript" src="js/SmoothScroll.js"></script>
+   <script src="js/main.js"></script>
+</body>
+
+</html>
